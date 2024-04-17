0da1d2bcb8a7bb4f53560e418cc638a416bf46a4
cristian m <cristian.miranda2023@twk.cl>
2024-04-17 18:30:54 -0400

    probando git 1

--- SistemaGestion/Program.cs
+++ SistemaGestion/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Probando git");
+Console.WriteLine("Probando git2");
+Console.WriteLine("Probando git3");
+Console.WriteLine("Probando git4");
