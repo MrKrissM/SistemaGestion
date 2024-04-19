2288acfc6280bbb5bb903ab14086998f21932a0f
cristian m <cristian.miranda2023@twk.cl>
2024-04-19 16:44:23 -0400

    eliminando cositas

--- SistemaGestion/Program.cs
+++ SistemaGestion/Program.cs
@@ -5,8 +5,3 @@ Console.WriteLine("Probando git");
 Console.WriteLine("Probando git2");
 Console.WriteLine("Probando git3");
 Console.WriteLine("Probando git4");
-
-Console.WriteLine("Probando git5");
-Console.WriteLine("Probando git6");
-Console.WriteLine("Probando git7");
-Console.WriteLine("Probando git8");
